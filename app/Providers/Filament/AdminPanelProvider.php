06f5f67e04e8579a6a953a783b7d44336c00dbad
Ade Irfan Imanda <adeheaven09@gmail.com>
2025-10-01 18:15:54 +0700

    Enhance AdminPanelProvider with navigation groups and sidebar settings
    - Updated AdminPanelProvider to include collapsible sidebar functionality on desktop.
    - Changed primary color from Amber to Red for the admin panel.
    - Added navigation groups for 'Data Wilayah' and 'Koperasi' with corresponding icons to improve organization and accessibility in the admin interface.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -28,8 +29,17 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->sidebarCollapsibleOnDesktop()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Red,
+            ])
+            ->navigationGroups([
+                NavigationGroup::make()
+                    ->label('Data Wilayah')
+                    ->icon('heroicon-o-map'),
+                NavigationGroup::make()
+                    ->label('Koperasi')
+                    ->icon('heroicon-o-building-office-2')
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
